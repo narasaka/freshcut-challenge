c21c909b78580fc09c0959a0f5b43271395d2467
Nathanael Tehilla <61931072+narasaka@users.noreply.github.com>
2022-07-09 07:41:24 -0700

    feat: video controls

--- src/ui/feed/Video.tsx
+++ src/ui/feed/Video.tsx
@@ -1,4 +1,10 @@
 import { ComponentPropsWithRef, useEffect, useState } from "react";
+import {
+  BsFillVolumeUpFill,
+  BsPauseFill,
+  BsPlayFill,
+  BsVolumeMuteFill,
+} from "react-icons/bs";
 import ReactPlayer from "react-player/lazy";
 import { User } from "../../types";
 import VideoCaption from "./VideoCaption";
@@ -23,6 +29,8 @@ const Video: React.FC<VideoProps> = ({
   isLiked,
 }) => {
   const [hasWindow, setHasWindow] = useState(false);
+  const [isMuted, setIsMuted] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(true);
   useEffect(() => {
     if (typeof window !== "undefined") {
       setHasWindow(true);
@@ -32,9 +40,52 @@ const Video: React.FC<VideoProps> = ({
     <>
       {hasWindow && (
         <div
-          className={`flex flex-col md:rounded-xl lg:flex-row bg-neutral-900 overflow-hidden ${className}`}
+          className={`group flex flex-col md:rounded-xl lg:flex-row bg-neutral-900 overflow-hidden ${className}`}
         >
-          <ReactPlayer url={url} width={"auto"} height={"auto"} />
+          <div className="relative">
+            <div className="absolute z-10 invisible group-hover:visible top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              {isPlaying ? (
+                <BsPauseFill
+                  color="white"
+                  size={72}
+                  onClick={() => setIsPlaying((state) => !state)}
+                />
+              ) : (
+                <BsPlayFill
+                  color="white"
+                  size={72}
+                  onClick={() => setIsPlaying((state) => !state)}
+                />
+              )}
+            </div>
+            <div className="absolute z-10 invisible group-hover:visible bottom-10 right-10">
+              {isMuted ? (
+                <BsVolumeMuteFill
+                  color="white"
+                  size={36}
+                  onClick={() => setIsMuted((state) => !state)}
+                />
+              ) : (
+                <BsFillVolumeUpFill
+                  color="white"
+                  size={36}
+                  onClick={() => setIsMuted((state) => !state)}
+                />
+              )}
+            </div>
+            <div
+              className="bg-black z-0 opacity-0 group-hover:opacity-50 absolute top-0 left-0 right-0 bottom-0"
+              onClick={() => setIsPlaying((state) => !state)}
+            ></div>
+            <ReactPlayer
+              url={url}
+              width={"auto"}
+              height={"auto"}
+              muted={isMuted}
+              playing={isPlaying}
+              loop
+            />
+          </div>
           <div className="lg:w-[58rem]">
             <VideoCaption user={user} title={title} />
             <VideoComments likes={likes} shares={shares} isLiked={isLiked} />
